90a909cf6ff02425c765c63fe2ac1ecc9cd2cbb3
Manfred Moser <manfred@simpligility.com>
2013-05-16 15:41:48 -0700

    more content and setup for creation to work

--- presentation/generate.sh
+++ presentation/generate.sh
@@ -6,7 +6,9 @@ mkdir -p target
 cp -R stylesheets target/
 cp -R javascripts target/
 cp -R images target/
-asciidoc --backend slidy --attribute theme=simpligility-slides --attribute stylesdir=`pwd`/stylesheets --attribute scriptsdir=`pwd`/javascripts -o target/ci-intro-android.html src/ci-intro-android.asciidoc
+asciidoc --backend slidy --attribute stylesdir=`pwd`/stylesheets --attribute scriptsdir=`pwd`/javascripts -o target/ci-intro-android.html src/ci-intro-android.asciidoc
+
+#asciidoc --backend slidy --attribute theme=simpligility-slides --attribute stylesdir=`pwd`/stylesheets --attribute scriptsdir=`pwd`/javascripts -o target/ci-intro-android.html src/ci-intro-android.asciidoc
 #rsync -avz src/images target
 
 #asciidoc -o target/ci-intro-android.html src/ci-intro-android.asciidoc
